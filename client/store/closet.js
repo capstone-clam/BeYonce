a57ef7e8ac27b1511f88b148f2cc77eba4211023
Macarena Carreno <macarenacarreno@Macarenas-MacBook-Pro.local>
2019-05-04 18:25:14 -0400

    update Dummy Data, add Store: Closet

---
+++ client/store/closet.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_CLOSET = 'GET_CLOSET'
+
+/**
+ * INITIAL STATE
+ */
+const closet = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getCloset = item => ({type: GET_CLOSET, item})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchCloset = () => async dispatch => {
+  try {
+    const res = await axios.get('/closet')
+    dispatch(getCloset(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = closet, action) {
+  switch (action.type) {
+    case GET_CLOSET:
+      return action.item
+    default:
+      return state
+      
+  }
+}
