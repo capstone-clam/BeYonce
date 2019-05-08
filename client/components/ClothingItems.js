076274cbb4cc1aec929c0c55594666bf26134c8f
Linda Saraguro <lindasaraguro@yahoo.com>
2019-05-07 22:56:28 -0400

    user will be rerouted to main closet view after selecting an item

--- client/components/ClothingItems.js
+++ client/components/ClothingItems.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {fetchCategory, addSelectedItem} from '../store'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import { Redirect } from 'react-router'
 
 export class ClothingItems extends Component {
   constructor(props) {
@@ -9,9 +10,10 @@ export class ClothingItems extends Component {
     this.state = {
       redirect: false
     }
-    this.findItems = this.findItems.bind(this)
+    // this.findItems = this.findItems.bind(this)
     this.handleClick = this.handleClick.bind(this)
-    this.renderRedirect = this.renderRedirect.bind(this)
+    // this.renderRedirect = this.renderRedirect.bind(this)
+    // this.handleOnMouseDown= this.handleOnMouseDown.bind(this)
   }
 
   componentDidMount() {
@@ -19,42 +21,49 @@ export class ClothingItems extends Component {
     this.props.fetchCategory(categoryId)
   }
 
-  findItems(category) {
-    if (category.inventories !== undefined) {
-      const inventories = category.inventories
-      return inventories
-    }
-  }
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Link to="/closet/"> </Link>
-    }
-  }
+  // findItems(category) {
+  //   if (category.inventories !== undefined) {
+  //     const inventories = category.inventories
+  //     return inventories
+  //   }
+  // }
+  // renderRedirect = () => {
+  //     return <Link to="/closet"> </Link>
+  // }
   handleClick(ev) {
     ev.preventDefault()
     let mySelection = Number(ev.target.id)
     this.props.addSelectedItem(mySelection)
     this.setState({redirect: true})
     console.log('STATE', this.state)
-    this.renderRedirect()
+    console.log("HITS ONCLICK")
+    // this.renderRedirect()
   }
 
+  // handleOnMouseDown(){
+  //   // this.renderRedirect()
+  //   console.log("HITS ONMOUSE!!")
+  //   return <Redirect to='/closet' />
+  //   // this.handleOnMouseDown
+  // }
   render() {
     const {inventories} = this.props
 
     return (
       <div id="closet-details">
         <h1 id="closeth1">BEYONCÉ CLOSET</h1>
-        <p id="closetp">Please choose up to one of each</p>
+        <p id="closetp">Please select one</p>
 
         {inventories.map(inventory => (
-          <div id="singlepic" key={inventory.id} onClick={this.handleClick}>
+          <div id="singlepic" key={inventory.id} onClick={this.handleClick} >
+            <Link to='/closet'>
             <img
               className="closetpics"
               src={inventory.filePath}
               id={inventory.id}
               value={inventory.item}
             />
+            </Link>
           </div>
         ))}
       </div>
