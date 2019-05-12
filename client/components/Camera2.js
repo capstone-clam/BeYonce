972bdce9317b35305b14c4a269fcd7571a1b9bf6
Linda Saraguro <lindasaraguro@yahoo.com>
2019-05-11 20:53:58 -0400

    changes

--- client/components/Camera2.js
+++ client/components/Camera2.js
@@ -1,13 +1,30 @@
-import {drawSkeleton, placeHat, placeGrammy} from './cameraUtils'
-// import React, {Component} from 'react'
+import React, {Component} from 'react'
+import Webcam, {
+  onUserMedia,
+  onUserMediaError,
+  audioConstraints
+} from 'react-webcam'
 import * as posenet from '@tensorflow-models/posenet'
+import {drawSkeleton, placeHat} from './cameraUtils'
 import {Closet} from '../components'
 import {connect} from 'react-redux'
 
-import React from 'react'
-import Webcam from 'react-webcam'
+class Camera2 extends Component {
+  //   static defaultProps= {
+  //     className: '',
+  //     audio:true,
+  //     height: 480, //	height of video element
+  //     width:	640,	// width of video element
+  //     screenshotWidth: 640, // width of screenshot
+  //     style: {},	//style prop passed to video element
+  //     screenshotFormat: '', //'image/webp'	format of screenshot
+  //     onUserMedia, //	function callback for when component receives a media stream
+  //     onUserMediaError, //function callback for when component can't receive a media stream with MediaStreamError param
+  //     screenshotQuality: 0.92,	//quality of screenshot(0 to 1)
+  //     audioConstraints,	//object, MediaStreamConstraint(s) for the audio
+  //     videoConstraints //	object, MediaStreamConstraints(s) for the video
+  // }
 
-class Camera2 extends React.Component {
   setRef = webcam => {
     this.webcam = webcam
   }
@@ -28,18 +45,19 @@ class Camera2 extends React.Component {
 
     return (
       <div>
+        <button type="button" onClick={this.capture}>
+          Capture photo
+        </button>
         <Webcam
-        id="webcam"
+          id="webcam"
           audio={false}
           height={1300}
           ref={this.setRef}
           screenshotFormat="image/jpeg"
           width={1300}
           videoConstraints={videoConstraints}
+          video={this.getvideo}
         />
-        <button type="button" onClick={this.capture}>
-          Capture photo
-        </button>
         <Closet />
         <img id="photo" />
         {selection.item ? (
@@ -53,42 +71,6 @@ class Camera2 extends React.Component {
     )
   }
 }
-// class Camera2 extends React.Component {
-//     // static defaultProps= {
-//     //     className: '',
-//     //     audio:true,
-//     //     height: 480, //	height of video element
-//     //     width:	640,	// width of video element
-//     //     screenshotWidth: 640, // width of screenshot
-//     //     style: {},	//style prop passed to video element
-//     //     screenshotFormat: '', //'image/webp'	format of screenshot
-//     //     onUserMedia, //	function callback for when component receives a media stream
-//     //     onUserMediaError, //function callback for when component can't receive a media stream with MediaStreamError param
-//     //     screenshotQuality: 0.92,	//quality of screenshot(0 to 1)
-//     //     audioConstraints,	//object, MediaStreamConstraint(s) for the audio
-//     //     videoConstraints //	object, MediaStreamConstraints(s) for the video
-//     // }
-
-//   render() {
-//     const {selection} = this.props.selection
-//     return(
-//       <div>
-//         <div>
-//           <Webcam className="webcam"/>;
-//           {selection.item ? (
-//             <img id="hat" src={selection.filePath} alt={selection.item} />
-//           ) : (
-//             <img id="hat" src="" alt="" />
-//           )}
-
-//           {/* <img id="grammy" src="/Grammycropped.png" /> */}
-//           {/* <img id="bodySuit" src="/BeyBarbieBodysuit.png" /> */}
-//         </div>
-//         <Closet />
-//       </div>
-//     )
-//   }
-// }
 
 const mapStateToProps = state => {
   return {
@@ -97,10 +79,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(Camera2)
-// const mapStateToProps = state => {
-//     return {
-//       selection: state.selection
-//     }
-//   }
-
-//   export default connect(mapStateToProps)(Camera2)
