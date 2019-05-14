c9d51c549d9e7785393bacb55bdb1e15c8a6fcf7
Alex Grazier <kylealexandra@gmail.com>
2019-05-14 15:20:28 -0400

    refactors song component, creates own controls with matching style to camerabutton

--- client/components/Songs.js
+++ client/components/Songs.js
@@ -2,29 +2,59 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchSongs} from '../store'
 import Fab from '@material-ui/core/Fab'
-// import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
 
+const audio = document.createElement('audio')
+audio.src = '/music/Formation.mp3'
+
 class Songs extends Component {
-  componentDidMount() {
-    this.props.fetchSongs(3)
+  constructor() {
+    super()
+    this.state = {
+      isPlaying: false
+    }
+    this.play = this.play.bind(this)
+    this.pause = this.pause.bind(this)
   }
 
+  play() {
+    audio.play()
+    this.setState({isPlaying: true})
+  }
+
+  pause() {
+    audio.pause()
+    this.setState({isPlaying: false})
+  }
+
+  // componentDidMount() {
+  //   this.props.fetchSongs(3)
+  // }
+
   render() {
-    const {inventories, loadingSongs} = this.props
-    if (loadingSongs) return <div>Loading Songs...</div>
+    // const {inventories, loadingSongs} = this.props
+    // if (loadingSongs) return <div>Loading Songs...</div>
     return (
       <div className="musicButton">
-        <Fab size="medium" color="secondary" aria-label="musicNote">
-          <Icon>music_note</Icon>
-          <audio src="/music/Formation.mp3" id="songinmain" />
+        <Fab
+          size="medium"
+          color="secondary"
+          aria-label="musicNote"
+          id="playButton"
+          onClick={this.play}
+        >
+          <Icon>play_arrow</Icon>
+        </Fab>
+        <Fab
+          size="medium"
+          color="secondary"
+          aria-label="musicNote"
+          id="stopButton"
+          onClick={this.pause}
+        >
+          <Icon>pause</Icon>
         </Fab>
       </div>
-
-      // <Button variant="contained" size="small" color="secondary">
-      // {/* <Icon>music_note</Icon> */}
-      // <audio src="/music/Formation.mp3" id="songinmain" />
-      // {/* </Button> */}
     )
   }
 }
