feb74b3ba4b4b8fd0bce73f517ef29d8687dbeb2
Alex Grazier <kylealexandra@gmail.com>
2019-05-14 13:49:39 -0400

    adds song button, not yet connected to song, adds styling to musicButton div to position bottom left

--- client/components/Songs.js
+++ client/components/Songs.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchSongs} from '../store'
+import Fab from '@material-ui/core/Fab'
+// import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon'
 
 class Songs extends Component {
   componentDidMount() {
@@ -11,9 +14,17 @@ class Songs extends Component {
     const {inventories, loadingSongs} = this.props
     if (loadingSongs) return <div>Loading Songs...</div>
     return (
-      <button>
-        <audio src="/music/Formation.mp3" id="songinmain" />
-      </button>
+      <div className="musicButton">
+        <Fab size="medium" color="secondary" aria-label="Camera">
+          <Icon>music_note</Icon>
+          <audio src="/music/Formation.mp3" id="songinmain" />
+        </Fab>
+      </div>
+
+      // <Button variant="contained" size="small" color="secondary">
+      // {/* <Icon>music_note</Icon> */}
+      // <audio src="/music/Formation.mp3" id="songinmain" />
+      // {/* </Button> */}
     )
   }
 }
